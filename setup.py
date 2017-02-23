c91a671a6f925dbc691520bda7f495560d1f105d
Erik Tollerud <erik.tollerud@gmail.com>
2017-02-23 16:22:02 -0500

    baseline setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='DEMUD',
+      version='0.1',
+      description='Python Distribution Utilities',
+      author='Kiri Wagstaff',
+      author_email='kiri.wagstaff@jpl.nasa.gov',
+      url='https://github.com/wkiri/DEMUD',
+      packages=['demud'],
+     )
