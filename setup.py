7331efdf44b2bc588b3589b1bd62c54cbddb40e6
Kiri Wagstaff <wkiri@wkiri.com>
2022-10-04 12:50:06 -0700

    Add description, submodules, and Python 3 requirement (#23)

--- setup.py
+++ setup.py
@@ -4,21 +4,21 @@ from setuptools import setup
 
 exec(open('demud/version.py').read())  # brings in a "version" variable
 
-description = ''
 with open('README.md') as f:
     long_description = f.read()
 
 setup(name='DEMUD',
       version=version,
-      description=description,
+      description='DEMUD novelty detection',
       long_description=long_description,
       author='Kiri Wagstaff',
       author_email='kiri.wagstaff@jpl.nasa.gov',
       url='https://github.com/wkiri/DEMUD',
-      packages=['demud'],
+      packages=['demud', 'demud.dataset', 'demud.log'],
       install_requires=['matplotlib',
                         'numpy',
                         'scipy',
                         'pillow'],
-      entry_points={'console_scripts': ['demud=demud.demud:main']}
+      entry_points={'console_scripts': ['demud=demud.demud:main']},
+      python_requires='>=3.0',
      )
