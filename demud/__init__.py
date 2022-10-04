f78c1c21cc01055bf2825ad3b8807191b7c30439
Kiri Wagstaff <wkiri@wkiri.com>
2022-10-04 12:44:55 -0700

    Share submodules (#23)

--- demud/__init__.py
+++ demud/__init__.py
@@ -1,4 +1,4 @@
-__all__ = [ "demud", '__version__']
+__all__ = [ "demud", '__version__', 'dataset', 'log']
 
-from demud import check_if_files_exist, load_data, parse_config
-from version import version as __version__
+#from demud import check_if_files_exist, load_data, parse_config
+#from version import version as __version__
